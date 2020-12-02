9e0649f25251eeeed40e87f2ddf48fc0f645d6e7
unknown <xiongjoh@gmail.com>
2020-12-02 16:15:02 -0600

    finish CRUD operations and middleware for userRouter.js

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const userDb = require('./userDb')
+const postDb = require('../posts/postDb')
 
 const router = express.Router();
 
 router.post('/', validateUser, async (req, res) => {
-  console.log(req.body)
-  const newUser = await userDb.insert(req.body)
-  res.status(201).json(newUser)
+  try {
+    const newUser = await userDb.insert(req.body)
+    res.status(201).json(newUser)
+  }
+  catch(error) {
+    res.status(500).json({message:error.message})
+  }
+
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validateUserId, validatePost, async (req, res) => {
+  const { id } = req.params
+  try {
+    const newPost = await postDb.insert({...req.body, user_id:id})
+    res.status(201).json(newPost)
+  }
+  catch(error) {
+    res.status(500).json({message:error.message})
+  }
 });
 
 router.get('/', (req, res) => {
@@ -28,15 +41,40 @@ router.get('/:id', validateUserId, (req, res) => {
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
-  // do your magic!
+  const { id } = req.params
+  userDb.getUserPosts(id)
+  .then(results => {
+    res.status(200).json(results)
+  })
+  .catch(error => {
+    res.status(500).json({message:error.message})
+  })
 });
 
-router.delete('/:id', validateUserId, (req, res) => {
-  // do your magic!
+router.delete('/:id', validateUserId, async (req, res) => {
+  const { id } = req.params
+  try {
+    await userDb.remove(id)
+    res.json(req.userInfo)
+  }
+  catch(error) {
+    res.status(500).json({message:error.message})
+  }
 });
 
-router.put('/:id',validateUserId, (req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId, validateUser, (req, res) => {
+  const { id } = req.params
+  userDb.update(id, req.body)
+  .then(results => {
+    if (results) {
+      res.status(201).json({id:Number(id), name:req.body.name})
+    } else {
+      res.status(400).json({message:"Failed to update database"})
+    }
+  })
+  .catch(error => {
+    res.status(500).json({message:message.error})
+  })
 });
 
 
@@ -71,7 +109,13 @@ async function validateUser(req, res, next) {
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if(!req.body) {
+    res.status(400).json({message:"missing post data"})
+  } else if (!req.body.text) {
+    res.status(400).json({message:"missing required text field"})
+  } else {
+    next()
+  }
 }
 
 module.exports = router;
