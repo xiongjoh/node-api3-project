50b3f09a40daf21b1fe31b288e178982cfe71541
unknown <xiongjoh@gmail.com>
2020-12-02 15:09:24 -0600

    logger middleware complete

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+server.use(logger)
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req.method, req.url, new Date(Date.now()))
+  next()
+}
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
 
 module.exports = server;
